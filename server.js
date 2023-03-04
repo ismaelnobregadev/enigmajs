e2964adb4bb5d0a1dd05170854ed5bb208ce44f7
Ismael Nóbrega <ismaelnobregadev@gmail.com>
2023-03-04 17:33:53 -0300

    atualizado server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const schema = require('enigma.js/schemas/12.20.0.json');
 // create a new session:
 const session = enigma.create({
   schema,
-  url: 'wss://qap.sebrae.com.br/app/engineData',
+  url: 'wss://qap.sebrae.com.br/app/96a7d192-860d-4ecc-90e7-a404e81154b2',
   createSocket: url => new WebSocket(url),
 });
 
@@ -16,8 +16,8 @@ session.on('traffic:received', data => console.log('received:', data));
 // open the socket and eventually receive the QIX global API, and then close
 // the session:
 session.open()
-  .then(global => global.engineVersion())
-  .then(result => console.log(result.qComponentVersion))
+  .then((qix) => qix.openDoc('96a7d192-860d-4ecc-90e7-a404e81154b2')
+  .then(result => console.log(result)
   .then(() => session.close())
   .then(() => console.log('Session closed'))
   .catch(err => console.log('Something went wrong :(', err));
